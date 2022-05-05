1cf3ad15382e841741f5d65b0f7a2faad079b47c
kaelajohnson <rikulova@gmail.com>
2022-05-04 21:40:43 -0600

    set function to prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword(){
+  var passwordLength = prompt("Password length?", "8-128 characters");
+  console.log(passwordLength);
+  if(passwordLength >= 8 && passwordLength <=128){
+  }
+  else{
+    alert("Please enter a correct length")
+    writePassword()
+    
+  }
+  
+
+}
